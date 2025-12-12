8ace9434d6030b175cf8d7d541c2882338b03b3f
Yahor Babko <onee31415@gmail.com>
2025-12-12 22:33:07 +0300

    feat: add basic calculator

---
+++ src/components/calculation.js
@@ -0,0 +1,68 @@
+'use strict';
+
+function toRpnArray(expression) {
+  const tokens = expression.replace(/[()]/g, '').split(' ');
+
+  const stack = [];
+  const operators = [];
+
+  const priority = {
+    '+': 1,
+    '-': 1,
+    '*': 2,
+    '/': 2
+  };
+
+  for (let token of tokens) {
+    if (!isNaN(token)) {
+      stack.push(token);
+    } else {
+      while (
+        operators.length > 0 &&
+        priority[operators[operators.length - 1]] >= priority[token]
+      ) {
+        stack.push(operators.pop());
+      }
+      operators.push(token);
+    }
+  }
+
+  while (operators.length > 0) {
+    stack.push(operators.pop());
+  }
+
+  return stack;
+}
+
+export default function calculate(expression) {
+  const tokens = toRpnArray(expression);
+  const stack = [];
+
+  for (let token of tokens) {
+    if (!isNaN(token)) {
+      stack.push(token);
+    } else {
+      const operand2 = stack.pop();
+      const operand1 = stack.pop();
+
+      switch (token) {
+        case '+':
+          stack.push(operand1 + operand2);
+          break;
+        case '-':
+          stack.push(operand1 - operand2);
+          break;
+        case '*':
+          stack.push(operand1 * operand2);
+          break;
+        case '/':
+          stack.push(operand1 / operand2);
+          break;
+      }
+    }
+  }
+
+  const result = stack.pop();
+
+  return isNaN(result) ? '0' : result;
+}
